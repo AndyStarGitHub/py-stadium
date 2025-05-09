52a12f1344a5a385506178af6c9abea1ee744468
AndyStar <andy_star@ukr.net>
2025-05-09 13:42:47 +0300

    Initial migration

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'stadium',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
--- stadium/admin.py
+++ stadium/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import (
+    SportArena,
+    Section,
+    Genre,
+    Actor,
+    Event,
+    EventSession,
+    Order,
+    Ticket,
+)
+
+admin.site.register(SportArena)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Section)
+admin.site.register(Event)
+admin.site.register(EventSession)
+admin.site.register(Order)
+admin.site.register(Ticket)
+
