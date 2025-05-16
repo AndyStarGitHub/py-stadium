c1a252cb3ae54eb0ee9ef44682f3e11268341c3b
AndyStar <andy_star@ukr.net>
2025-05-16 16:28:33 +0300

    Secret key hidden

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from datetime import timedelta
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4ufq_e^s@&h^_tq1k0bk$7q-&7n@p0$&j@l92_*35u!^n!vl#j"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,18 +79,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB", "stadium"),
-        "USER": os.environ.get("POSTGRES_USER", "stadium"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "stadium"),
-        "HOST": os.environ.get("POSTGRES_HOST", "db"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": os.environ.get("POSTGRES_DB", "stadium"),
+    #     "USER": os.environ.get("POSTGRES_USER", "stadium"),
+    #     "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "stadium"),
+    #     "HOST": os.environ.get("POSTGRES_HOST", "db"),
+    #     "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+    # }
 }
 
 
